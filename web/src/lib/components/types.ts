d7dc1db1a08e160409f6b8c0cb674cb237402d99
Erik Schouten <erik@erikschouten.com>
2025-11-27 22:11:09 +0100

    feat: base dashboard

---
+++ web/src/lib/components/types.ts
@@ -0,0 +1,12 @@
+import type { Component } from "svelte";
+
+export type NavItem = {
+    title: string;
+    url: string;
+    icon: Component;
+}
+
+export type NavMenu = {
+    title: string;
+    items: NavItem[];
+}
