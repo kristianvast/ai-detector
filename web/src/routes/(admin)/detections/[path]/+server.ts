86cfd57ce0e8749be45fd8e333690c6e51f54d36
Erik Schouten <erik@erikschouten.com>
2025-12-01 21:39:24 +0100

    fix: async

--- web/src/routes/(admin)/detections/[path]/+server.ts
+++ web/src/routes/(admin)/detections/[path]/+server.ts
@@ -1,10 +1,10 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import path from 'node:path';
 
 export async function GET({ params }) {
     const detections = path.resolve('detections');
     const imgPath = path.join(detections, params.path);
-	return new Response(fs.readFileSync(imgPath), {
+	return new Response(await fs.readFile(imgPath), {
 		headers: {
 			'Content-Type': 'image/jpeg'
 		}
