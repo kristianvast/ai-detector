6021efd67a411a2575e7e97a6259392ddb60e931
Erik Schouten <erik@erikschouten.com>
2025-12-08 09:38:21 +0100

    fix: updated dll hooks

--- detector/hooks/hook-llama_cpp.py
+++ detector/hooks/hook-llama_cpp.py
@@ -1,15 +1,5 @@
-import glob
-import os
+from PyInstaller.utils.hooks import collect_dynamic_libs
 
-from PyInstaller.utils.hooks import get_package_paths
-
-# Get the package path
-pkg_base, pkg_dir = get_package_paths("llama_cpp")
-
-# Find all DLLs in the package directory
-dlls = glob.glob(os.path.join(pkg_dir, "*.dll"))
-
-# Add them as binaries, preserving the folder structure (putting them inside llama_cpp in the bundle)
-binaries = []
-for dll in dlls:
-    binaries.append((dll, "llama_cpp"))
+# Automatically collect all shared libraries
+binaries = collect_dynamic_libs("llama_cpp")
+print(f"🚀 Hook executed at compile time: {binaries}")
