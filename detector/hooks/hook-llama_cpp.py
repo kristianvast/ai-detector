d0147bf557053c6137a722c39a58b7ff7ba8ded7
Erik Schouten <erik@erikschouten.com>
2025-12-08 09:56:14 +0100

    fix: do not print binary

--- detector/hooks/hook-llama_cpp.py
+++ detector/hooks/hook-llama_cpp.py
@@ -2,4 +2,4 @@ from PyInstaller.utils.hooks import collect_dynamic_libs
 
 # Automatically collect all shared libraries
 binaries = collect_dynamic_libs("llama_cpp")
-print(f"🚀 Hook executed at compile time: {binaries}")
+print(f"Hook executed at compile time: {len(binaries)} binaries collected")
