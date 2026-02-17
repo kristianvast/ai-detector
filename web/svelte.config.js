c00635b7260ad3dfab69a3bbac23711a03632b77
Erik Schouten <erik@erikschouten.com>
2026-02-18 00:20:49 +0100

    fix: baseline

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			binaryName: "ai-detector-web",
-			// target: "windows-x64-baseline"
+			target: "windows-x64-baseline"
 		}),
 		experimental: {
 			remoteFunctions: true
