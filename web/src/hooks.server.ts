35191599e220a5f8716dfbe43ef607be8541dc0f
Erik Schouten <erik@erikschouten.com>
2026-04-15 09:56:35 +0200

    fix: exe fix

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,3 +1,9 @@
+await import('reflect-metadata');
+
+if (typeof Reflect === 'undefined' || typeof Reflect.getMetadata !== 'function') {
+	throw new Error('Failed to initialize reflect-metadata for server startup.');
+}
+
 import { getConfig } from "$lib/remote/config.remote";
 
 // getConfig();
